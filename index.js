f4b82bb5fa23f48eea3815d978ca92db311239c4
Ed Wright <ew_wrightiii@live.com>
2017-12-14 13:03:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+//mutates program state, does not produce new array
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return name
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return name
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+//non mutate array returns new array, leaving kittens unchanged
+function appendKitten(name) {
+  var newArray = [...kittens]
+  newArray.push(name)
+  return newArray
+}
+
+function prependKitten(name) {
+  var newArray = [...kittens]
+   newArray.unshift(name)
+  return newArray
+}
+
